50e8ac1cdf0cc5939e32f35113e78bff22f65bba
Morten Kjetland <mbk@kjetland.com>
2015-06-23 14:14:18 +0200

    Added some tests to booking example

---
+++ examples/aggregates/src/test/scala/no/nextgentel/oss/akkatools/example/booking/BookingStateTest.scala
@@ -0,0 +1,40 @@
+package no.nextgentel.oss.akkatools.example.booking
+
+import org.scalatest.{FunSuite, Matchers}
+
+class BookingStateTest extends FunSuite with Matchers {
+
+  test("Booking before open should fail") {
+    val s = BookingState.empty()
+
+    val e = ReservationEvent("id1")
+
+    val error = intercept[BookingError] {
+      s.transition(e)
+    }
+    assert(error.getMessage == "Invalid event since Booking is not opened yet")
+  }
+
+  test("Normal flow") {
+    var s = BookingState.empty()
+
+    s = s.transition(BookingOpenEvent(2))
+    s = s.transition(ReservationEvent("1"))
+    s = s.transition(ReservationEvent("2"))
+
+    // This one should fail - no more roome
+    val error = intercept[BookingError] {
+      s.transition(ReservationEvent("2"))
+    }
+    assert(error.getMessage == "No more seats available")
+
+    s = s.transition(CancelationEvent("1"))
+    s = s.transition(ReservationEvent("3"))
+    s = s.transition(BookingClosedEvent())
+
+    assert( s == BookingState(StateName.CLOSED, 2, Set("2", "3")))
+
+
+  }
+
+}
