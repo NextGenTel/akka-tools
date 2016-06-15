e5a94ea1acb8ab72e40fa79a3958f5e736d21955
Morten Kjetland <mbk@kjetland.com>
2016-06-15 10:15:52 +0200

    Fixing java-compile-issue related to previous commit

--- akka-tools-persistence/src/test/java/no/nextgentel/oss/akkatools/aggregate/JavaError.java
+++ akka-tools-persistence/src/test/java/no/nextgentel/oss/akkatools/aggregate/JavaError.java
@@ -3,6 +3,10 @@ package no.nextgentel.oss.akkatools.aggregate;
 public class JavaError extends AggregateError {
 
     public JavaError(String errorMsg) {
-        super(errorMsg);
+        super(errorMsg, false);
+    }
+
+    public JavaError(String errorMsg, boolean skipErrorHandler) {
+        super(errorMsg, skipErrorHandler);
     }
 }
